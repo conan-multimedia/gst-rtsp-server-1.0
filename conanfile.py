9b9cc9ee8f806d4d712529fcc1f4d6eecbca908d
cppbitman <chinadaihongjun@gmail.com>
2018-11-08 23:09:17 -0800

    remove unneeded comments

--- conanfile.py
+++ conanfile.py
@@ -12,9 +12,6 @@ class GstrtspserverConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=True"
     generators = "cmake"
-    #requires = ("gstreamer/1.14.3@bincrafters/stable", "gst-plugins-base/1.14.3@bincrafters/stable",
-    #"gobject-introspection/1.54.1@bincrafters/stable","libffi/3.99999@user/channel","gst-plugins-good/1.14.3@bincrafters/stable",
-    #"gst-plugins-bad/1.14.3@bincrafters/stable")
     requires = ("gstreamer-1.0/1.14.4@conanos/dev","gst-plugins-base-1.0/1.14.4@conanos/dev",
                 "gst-plugins-good-1.0/1.14.4@conanos/dev","gst-plugins-bad-1.0/1.14.4@conanos/dev",
                 "gobject-introspection/1.58.0@conanos/dev","glib/2.58.0@conanos/dev",
@@ -27,22 +24,6 @@ class GstrtspserverConan(ConanFile):
         os.rename(extracted_dir, self.source_subfolder)
 
     def build(self):
-        #vars = {'PKG_CONFIG_PATH': "%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig"
-        #%(self.deps_cpp_info["gstreamer"].rootpath,self.deps_cpp_info["gst-plugins-base"].rootpath,
-        #self.deps_cpp_info["gobject-introspection"].rootpath,self.deps_cpp_info["libffi"].rootpath)}
-        #
-        #with tools.environment_append(vars):
-        #    self.run('sh ./autogen.sh --noconfigure && ./configure --prefix %s/build'
-        #    ' --libdir %s/build/lib --enable-introspection'
-        #    ' --disable-examples --enable-static'%(os.getcwd(),os.getcwd()))
-        #    self.run('make -j4')
-        #    self.run('make install')
-        #vars = {
-        #        #'LD_LIBRARY_PATH':'%s/lib:%s/lib'%(self.deps_cpp_info["libffi"].rootpath,self.deps_cpp_info["glib"].rootpath),
-        #        'PATH':'%s/bin:%s'%(self.deps_cpp_info["gobject-introspection"].rootpath, os.getenv("PATH"))
-        #       }
-        #with tools.environment_append(vars):
-
         with tools.chdir(self.source_subfolder):
             with tools.environment_append({
                 'PATH':'%s/bin:%s'%(self.deps_cpp_info["gobject-introspection"].rootpath, os.getenv("PATH")),
@@ -56,13 +37,7 @@ class GstrtspserverConan(ConanFile):
                          },
                     source_dir = '%s'%(os.getcwd()),
                     build_dir= '%s/builddir'%(os.getcwd()),
-                    pkg_config_paths=[ #'%s/lib/pkgconfig'%(self.deps_cpp_info["gstreamer"].rootpath),
-                                       #'%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-base"].rootpath),
-                                       #'%s/lib/pkgconfig'%(self.deps_cpp_info["gobject-introspection"].rootpath),
-                                       #'%s/lib/pkgconfig'%(self.deps_cpp_info["libffi"].rootpath),
-                                       #'%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-good"].rootpath),
-                                       #'%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-bad"].rootpath),
-                                       '%s/lib/pkgconfig'%(self.deps_cpp_info["gstreamer-1.0"].rootpath),
+                    pkg_config_paths=[ '%s/lib/pkgconfig'%(self.deps_cpp_info["gstreamer-1.0"].rootpath),
                                        '%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-base-1.0"].rootpath),
                                        '%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-good-1.0"].rootpath),
                                        '%s/lib/pkgconfig'%(self.deps_cpp_info["gst-plugins-bad-1.0"].rootpath),
